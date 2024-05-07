af88fdd3c99d485b6d2f6aeaf84d4e1eb12f83ef
Serki Yildirim <serki_yildirim.it@hotmail.com>
2024-05-07 19:47:31 +0200

    erster Commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4; 
+
+export const dialogueData = {
+    pc: `Das ist mein Laptop, den ich leider wieder abgeben musste. Hier habe ich meine Projekte programmiert. Ich habe hauptsächlich mit Html/CSS und 
+    JavaScript die Projekte erstellt. Ich bin im Allgemeinen sehr offen für neue Technologien und Prgrammiersprachen und lerne sie gerne.`,
+    "cs-degree": `... Manchmal findet man den wahren Frieden in der Stille. Und das ist der Ort hierfür.`,
+    "sofa-table": `Hier ist mein Sofa. wo ich mich entspanne und etwas kurdische Geige spiele und dazu singe. Hier ist ein Video von mir
+    <a href="https://www.youtube.com/watch?v=11MbNQFNHoQ" target="_blank"> >> Hier geht es zum Video <<.</a> ` ,
+    tv: `Hier ist mein Fernseher. Hier schaue ich unter anderem auch den Film <a href="https://www.youtube.com/watch?v=zdl01i3F1VA" target="_blank"> PRAKTI.COM </a>
+    Das ist eines meiner Lieblingsfilme.`,
+    bed: `Hier schlafe ich. Große Ideen kommen mir, wenn ich in meinem Bett liege. Wenn eine Idee mich trifft, muss ich sie oft aufschreiben, sonst kann ich nicht schlafen, weil meine geistige Energie davon verbraucht wird.`,
+    resume: `Hier befindet sich mein Schreibtisch wo ich an meine Projekte gearbeitet habe. Hier sind meine Projete, die ich bereits programmiert habe. <a href="https://github.com/serki-yildirim?tab=repositories" target="_blank">Github-Profil</a>
+    Gerne können Sie mich unter der folgenden E-Mail Adresse für ein Pflichtpraktikum von 6 Monaten kontaktieren.
+    E-Mail: serki_yildirim.it@hotmail.com`,
+    projects: `Informationen zu diesem Portfolio: Es wurde mit der Kaboom.js-Bibliothek erstellt, die eine Bibliothek zur Erstellung von Spielen in JavaScript ist. Der Text wird mit HTML/CSS gerendert.
+    Daher wird das Textfeld, das Sie gerade lesen, nicht innerhalb des Canvas gerendert.`,
+    library: `Ich lese unheimlich gerne Bücher über Informatik bis hin zu diversen Webtechnologien und Programmiersprachen und auch was das Mindset angeht im Kontext der Softwareentwicklung, ich habe bereits viele gelesen. Manchmal fällt es mir schwer, welches ich lesen soll ! 
+    <a href="https://www.instagram.com/p/CgnImoCKtAa/?next=%2F" target="_blank"> >> Meine Bibliothek <<</a>`,
+    exit: `Wenn du mein Portfolio verlassen willst, dann kannst du einfach den Tab schließen.`,
+  };
